30c7ac1b6ed6a5b228c0ecefc82b7123a61b6935
teo <teo@pruebas.uc3m.es>
2017-10-05 20:20:50 +0200

    modified:   lib/CiA402Device 	modified:   main.cpp

--- main.cpp
+++ main.cpp
@@ -10,10 +10,10 @@ int main(int argc, char *argv[])
     CiA402DeviceICanbus com;
     can_msg can_in,can;
     co_msg input, output;
-    uint8_t msg_start[]={};//={0x81,0x00};
-    vector<uint8_t> co_data_frame(msg_start,msg_start);
+    uint8_t msg_start[]={0x81,0x00};//={0x81,0x00};
+    vector<uint8_t> co_data_frame(msg_start,msg_start+2);
     //com.SetCanMsg(can,msg_start);
-    input=com.SetCanOpenMsg(0x702,1,co_data_frame);//0xE05
+    input=com.SetCanOpenMsg(0x00,0,co_data_frame);//0xE05
     //com.SetCanMsg(can,msg_start);
     //com.co2c(input,can);
 
@@ -21,11 +21,11 @@ int main(int argc, char *argv[])
         cout<<"error al enviar el mensaje"<<endl;
     }
     else{
-        for(int i=0; i<100; i++){
+        //for(int i=0; i<100; i++){
             if (com.WaitForReadMessage(output,0)<0){
                 cout<<"error al recibir el mensaje"<<endl;
             }
-        }
+        //}
     }
 
     return 0;
